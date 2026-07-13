93be99e7c8fdd95e74fac20965a2add7cfa94a3c
Sophia Aragão <sophiaaragaoll@gmail.com>
2026-07-13 08:30:30 -0300

    teste 0.2 speech-emotion-recognition
    datasets crema-d, tess, ravdess, savee

--- execute.sh
+++ execute.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 #SBATCH --job-name=speech-emotion-recognition
-#SBATCH --output=logs/results_%j.txt
-#SBATCH --error=logs/errors_%j.txt     
 #SBATCH --ntasks=1                     
 #SBATCH --cpus-per-task=8              
 #SBATCH --mem=32G                      
 #SBATCH --time=24:00:00                
 #SBATCH --partition=amd-512
 
-# 1. Criando a pasta de "logs", caso ela não exista.
-mkdir -p logs
-
-# 2. Inicializando o Conda no script.
-source $(conda info --base)/etc/profile.d/conda.sh
-
-# 3. Ativando o ambiente Conda.
+# Inicializando o ambiente Conda.
+eval "$(conda shell.bash hook)"
 conda activate speech-emotion-recognition
 
-# 4. Navegando para o diretório de trabalho onde o job foi submetido.
+# Acessando o diretório do Slurm.
 cd $SLURM_SUBMIT_DIR
 
-# 5. Executando o projeto.
-python main.py
+# Executando o script Python gerado.
+echo "Iniciando o treinamento..."
+python speech-emotion-recognition.py
