054ae163184d310914d49c7a53b469a845a1a815
Sophia Aragão <sophiaaragaoll@gmail.com>
2026-09-03 20:56:41 -0300

    cross-validation e implementação de 10 folds

--- execute.sh
+++ execute.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 #SBATCH --job-name=speech-emotion-recognition
-#SBATCH --ntasks=1                     
-#SBATCH --cpus-per-task=8              
-#SBATCH --mem=32G                      
-#SBATCH --time=24:00:00                
-#SBATCH --partition=amd-512
+#SBATCH --partition=gpu-8-v100
+#SBATCH --gpus-per-node=4
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=128G
+#SBATCH --time=3-0:0
 
 # Inicializando o ambiente Conda.
 eval "$(conda shell.bash hook)"
 conda activate speech-emotion-recognition
 
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
+
+CUDNN_PATH=$(dirname $(python -c "import nvidia.cudnn;print(nvidia.cudnn.__file__)" 2>/dev/null) 2>/dev/null)
+CUBLAS_PATH=$(dirname $(python -c "import nvidia.cublas;print(nvidia.cublas.__file__)" 2>/dev/null) 2>/dev/null)
+NVVM_PATH=$(dirname $(python -c "import nvidia.cuda_nvrtc;print(nvidia.cuda_nvrtc.__file__)" 2>/dev/null) 2>/dev/null)
+
+if [ ! -z "$CUDNN_PATH" ]; then export LD_LIBRARY_PATH=$CUDNN_PATH/lib:$LD_LIBRARY_PATH; fi
+if [ ! -z "$CUBLAS_PATH" ]; then export LD_LIBRARY_PATH=$CUBLAS_PATH/lib:$LD_LIBRARY_PATH; fi
+if [ ! -z "$NVVM_PATH" ]; then export LD_LIBRARY_PATH=$NVVM_PATH/lib:$LD_LIBRARY_PATH; fi
+
 # Acessando o diretório do Slurm.
 cd $SLURM_SUBMIT_DIR
 
+# Testando a conexão com a GPU.
+echo "Placas de vídeo alocadas:"
+nvidia-smi
+
 # Executando o script Python gerado.
-echo "Iniciando o treinamento..."
-python speech-emotion-recognition.py
+echo "Iniciando o treinamento na GPU..."
+python -u speech-emotion-recognition.py
